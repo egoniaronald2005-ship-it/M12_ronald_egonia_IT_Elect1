006f5af6e99c80c5971bcc3925ee65b1d0adfd48
egoniaronald2005-ship-it <egoniaronald2005@gmail.com>
2025-09-26 13:13:53 +0800

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import CounterApp from './CounterAppApp';
+import React from 'react';
+import {View} from 'react-native';
+import CounterApp from './CounterApp';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{flex: 1}}>
+      <midtermAct1App />
     </View>
-  );
-}
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
